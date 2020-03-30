af601b70a986f3c72a51245c29cfb2f865d09aa9
Gerson <gerson_0007@hotmail.com>
2020-03-30 14:08:04 -0400

    sadasda

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -8,11 +8,58 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Mostrar Alertas"),          
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: ()=>_mostrarAlert(context),
+          ),
+      ),
       floatingActionButton:FloatingActionButton(
         child: Icon(Icons.keyboard_backspace),
         onPressed: (){
           Navigator.pop(context);
         },),
     );
+  }
+
+  void _mostrarAlert(BuildContext context) {
+    
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context){
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(22.0)),
+          title: Text('contenido'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('este es el contenido de la caja de la alaerta'),
+              FlutterLogo(size:100.0)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('cancelar'),
+              onPressed: (){
+                Navigator.of(context).pop();
+               },
+            ),
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: (){ 
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      }
+
+      );
+
+
   }
 }
--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,6 +7,30 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://kabina34radio.com/wp-content/uploads/2019/05/URnaMrya.jpg'),
+              radius: 20.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.purple,
+              
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://elpais.com/elpais/imagenes/2018/11/13/gente/1542123826_040867_1542176366_noticia_fotograma.jpg'),
+          fadeInDuration: Duration(milliseconds: 200),
+          ),
       ),
     );
   }
